2a044dec9a12ec2f7ff151b6128e55ef288560d8
Vy Nguyen <vynguyen.web@gmail.com>
2013-08-16 13:14:00 -0700

    Content and css

--- wp-content/themes/bf_theme_base/front-page.php
+++ wp-content/themes/bf_theme_base/front-page.php
@@ -5,4 +5,38 @@
 		</div>
 	</div>
 
+	<section id="main-content">	
+
+		<div id="banner">
+			<div class="wrapper">
+				<p id="banner-img"><img src="images/banner.jpg"></p>
+				<h3 class="script-font">We're so glad you're here.</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit ligula risus, a dignissim lectus ornare ut. Praesent est odio, consectetur at consequat in, ullam id lacus. Etiam condimentum urna rhoncus tempus molestie. Duis tristique elit non est.</p>
+				<p id="new-to-us"><a href="">New to Us? Click Here</a></p>
+			</div>
+		</div>
+		<div class="wrapper">
+			<div class="col" id="left">
+				<h2 class="script-font">Worship</h2>
+				<p>SUNDAY SCHEDULE</p>
+				<p>09:15  -  Sunday School (Classrooms)</p>
+				<p>10:00  -  Main Service (Main Sanctuary)</p>
+				<p>11:30  -  Prayer Meeting (Room 7)</p>
+				<p>12:00  -  Lunch	(Main Hall)</p>
+				<a href="" class="colbutton">Listen to Sermons</a>
+			</div>
+			<div class="col" id="center">
+				<h2 class="script-font">Learn</h2>
+				<p>Donec suscipit ligula risus, a dignissim lectus ornare ut. Praesent est odio, consectetur at consequat in, ullamcorper id lacus. Etiam condimentum urna rhoncus tempus molestie. Duis tristique elit non est accumsan, vitae ullamcorper justo iaculis.</p>
+				<a href="" class="colbutton">Learn More</a>		
+			</div>
+			<div class="col" id="right">	
+				<h2 class="script-font">Serve</h2>
+				<p>Etiam condimentum urna rhoncus tempus molestie. Duis tristique elit non est accumsan, vitae ullamcorper justo iaculis. Velit rhoncus. consequat. Aliquam erat volutpat. Nam elit eros, placerat ut enim id, tincidunt venenatis dui. Curabitur non elit sollicitudin est, vel tincidunt magna. </p>
+				<a href="" class="colbutton">Learn More</a>		
+			</div>
+		</div>
+
+	</section>
+
 <?php get_footer(); ?>
